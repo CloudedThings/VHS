82c70db210190df832077f6ea06dfa2fa8a8bc2e
Leszek Ucinski <rahvinos@gmail.com>
2021-11-30 10:38:07 +0100

    Add project files.

---
+++ VHS/Controllers/StatusController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VHS.Entity;
+
+
+namespace VHSBackend.Web.Controllers
+{
+    [Route("api/vin")]
+    [ApiController]
+    public class StatusController : ControllerBase
+    {
+      //  public statuscontroller()
+      //  {
+      //      _statusrepository = new sqlstatusrepository();
+      // }
+
+        [HttpPost]
+        [Route("status")]
+        public ActionResult<bool> PostStatus(VIN vin)
+        {
+            return new OkObjectResult(vin);
+        }
+
+        [HttpPost]
+        [Route("greet")]
+        public ActionResult<string> Greet(string text)
+        {
+            return new OkObjectResult($"Hello {text}");
+        }
+
+
+    }
+
+
+}
---
+++ VHSBackend.Core/ServiceProvider.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VHSBackend.Core
+{
+    public class ServiceProvider
+    {
+        private static Services _services;
+
+        public static Services Current => _services ??= new Services();
+
+        public static Services Current2
+        {
+            get
+            {
+                if (_services == null)
+                {
+                    _services = new Services();
+                }
+
+                return _services;
+            }
+        }
+    }
+}
